f7c2bc57ce954a5233710bba8b4c240f9502d56d
Nemoto Mailon <minatokiroisen77@gmail.com>
2025-11-25 22:50:58 +0900

    startig MERN Auth tuto from backend setting up

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import dotenv from 'dotenv'
+const app = express()
+dotenv.config()
+
+const port = process.env.PORT || 5000
+
+app.get("/", (req, res) => {
+    res.send("Server is ready to serve Sir..")
+})
+
+app.listen(port, () => console.log("Server started on port", port))
+
+
+// POST / api / users - Register a User
+// POST /api/users/auth  - Authenticate a user and get token
+// POST /api/users/logout  - Logout user and clear cookie
+// GET  /api/users/profile  - Get user Profile
+// PUT  /api/users/profile  - Update profile
